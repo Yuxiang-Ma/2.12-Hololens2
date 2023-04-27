91ab37dfaa6a85bc445dd36017800711bf1337fb
youngp@mit.edu <youngp@mit.edu>
2023-04-27 12:43:46 -0400

    let clients to write torque data into the csv files and generate the corresponding images.

--- Comms/Mock/mock_hololens.py
+++ Comms/Mock/mock_hololens.py
@@ -1,15 +1,31 @@
+from typing import Dict
 import socket  # Import the socket library for network communication
 import time  # Import the time library for time-related functions
 import random  # Import the random library for generating random numbers and choices
 import struct  # Import the struct library for working with packed binary data
+import datetime
 
 # Replace the following with the IP address of the machine running the server
-server_ip = "10.29.122.25"  # Define the server IP address (localhost in this case)
-server_port = 21200  # Define the server port number
+SERVER_IP = "10.29.122.25"  # Define the server IP address (localhost in this case)
+SERVER_PORT = 21200  # Define the server port number
+
+# Path to save data
+SAVE_PATH = "../../data"
+
+def write_data(data_dict: Dict[str, float], verbose=True):
+    now_utc_stamp = datetime.datetime.now(tz=datetime.timezone.utc).timestamp()
+
+    for key, value in data_dict.items():
+        if verbose:
+            print(f"Received {key}:", key)
+
+        with open(f'./ur5_{key}.csv', 'a') as f:
+            f.write(f'{now_utc_stamp}, {value}\n')
+
 
 def main():  # Define the main function
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:  # Create a TCP socket
-        s.connect((server_ip, server_port))  # Connect to the server
+        s.connect((SERVER_IP, SERVER_PORT))  # Connect to the server
         print("Connected to server")  # Print a message indicating successful connection
 
         while True:  # Run an infinite loop
@@ -17,10 +33,15 @@ def main():  # Define the main function
             s.sendall(message.encode())  # Send the message to the server
 
             data = s.recv(1024)  # Receive data from the server (up to 1024 bytes)
+            # FIXME: Need to recieve not only torque but also displacement
             torques = struct.unpack("!" + "f" * 6, data)  # Unpack the received packed binary data into a tuple of 6 floats
-            print("Received torques:", torques)  # Print the received torques
+            data_dict = {
+                f'torque{i}': t for i, t in enumerate(torques)
+            }
+            write_data(data_dict, verbose=True)
 
             time.sleep(1)  # Wait for 1 second before sending the next message
+
 
 if __name__ == "__main__":
     main()  # Run the main function when the script is executed
--- PythonModules/config.py
+++ PythonModules/config.py
@@ -1,13 +1,21 @@
 
 # Specify the paths here
 DATA_PATHS = {
-    "displacement": "../data/ur5_displacement.csv",
-    "force": "../data/ur5_force.csv",
+    "torque0": "../data/ur5_torque0.csv",
+    "torque1": "../data/ur5_torque1.csv",
+    "torque2": "../data/ur5_torque2.csv",
+    "torque3": "../data/ur5_torque3.csv",
+    "torque4": "../data/ur5_torque4.csv",
+    "torque5": "../data/ur5_torque5.csv",
 }
 
 SAVE_PATHS = {
-    "displacement": "../img/ur5_displacement.png",
-    "force": "../img/ur5_force.png",
+    "torque0": "../img/ur5_torque0.png",
+    "torque1": "../img/ur5_torque1.png",
+    "torque2": "../img/ur5_torque2.png",
+    "torque3": "../img/ur5_torque3.png",
+    "torque4": "../img/ur5_torque4.png",
+    "torque5": "../img/ur5_torque5.png",
 }
 
 # Read
