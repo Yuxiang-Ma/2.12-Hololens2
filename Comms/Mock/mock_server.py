ede481511ffe0a741b60323b09fa47717a65115a
Malek <shmeek8@gmail.com>
2023-05-07 14:54:14 -0400

    Added functionality to receive images on the server using a writer from the asyncio module on the client computer. Changes have been made to the mock server code.

--- Comms/Mock/mock_server.py
+++ Comms/Mock/mock_server.py
@@ -18,6 +18,7 @@ The script continuously listens for messages from the HoloLens and processes the
 """
 
 import asyncio # Import the asyncio library for asynchronous I/O
+import socket # Import the socket library for determining IP address
 import concurrent.futures # Import the concurrent.futures library for thread-based parallelism
 import struct # Import the struct library for working with C-style data structures
 import time # Import the struct library for working with C-style data structures
@@ -29,6 +30,13 @@ torques = [0] * 6 # Initialize a list of 6 zeroes to store torque values for eac
 messages = ["DOWN"] # Initialize a list with the initial message "DOWN"
 executor = concurrent.futures.ThreadPoolExecutor(max_workers=1) # Create a ThreadPoolExecutor with a single worker
 
+def get_ip_address():
+    try:
+        hostname = socket.gethostname()
+        ip_address = socket.gethostbyname(hostname)
+        return ip_address
+    except:
+        return "Error: Unable to get IP address"
 
 def comm_robot():
     rtde_c = RTDEControlInterface("169.254.9.43") # Create a control interface with the robot's IP address
@@ -91,6 +99,8 @@ async def receive_image(reader: asyncio.StreamReader, writer: asyncio.StreamWrit
             break
 
 async def main():
+    print(f"Running on IP: {get_ip_address()}")  # Add this line
+
     loop = asyncio.get_running_loop()
     await asyncio.gather(
         comm_hololens(),
