2eacdc014c13cddc8b3283bbd5fd2fdef8eacbf2
youngp@mit.edu <youngp@mit.edu>
2023-05-08 17:40:17 -0400

    uploade ur5 image to the ip server

---
+++ Comms/IPServer/upload_image.py
@@ -0,0 +1,20 @@
+
+from flask import Flask, render_template, send_from_directory
+
+app = Flask(__name__)
+
+IMAGE_FOLDER = '../../img'
+
+
+@app.route(f'/uploads/display_image.png')
+def uploaded_file(filename='display_image.png'):
+    return send_from_directory(IMAGE_FOLDER, filename)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=4000, debug=True, threaded=True)
