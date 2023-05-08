b37da7258d45fc0d2df9d2d8e9722e03c9dec20f
malekinho8 <shmeek8@gmail.com>
2023-05-08 09:55:53 -0400

    Implemented more efficient video playing ability (taken from ChatGPT)

--- Assets/Scripts/WebStream.cs
+++ Assets/Scripts/WebStream.cs
@@ -4,14 +4,17 @@ using System.IO;
 using System.Net;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class WebStream : MonoBehaviour
 {
     public MeshRenderer frame;    //Mesh for displaying video
 
-    public string sourceURL = "http://10.189.3.42:3000";
+    public string sourceURL = "10.189.3.42:3000";
+    sourceURL = "http://" + sourceURL + "/stream"; // makes it easier to change the URL in the inspector
     private Texture2D texture;
     private Stream stream;
+    private VideoPlayer videoPlayer;
     public TextMeshPro textMesh = null;
 
     private TouchScreenKeyboard _urlKeyboard;
@@ -50,6 +53,7 @@ public class WebStream : MonoBehaviour
             if (sourceURL != newUrl)
             {
                 sourceURL = newUrl;
+                sourceURL = "http://" + sourceURL + "/stream"; // makes it easier to change the URL in the inspector
                 StartStream();
             }
         });
@@ -58,93 +62,90 @@ public class WebStream : MonoBehaviour
 
     private void StartStream()
     {
-        if (stream != null)
+        if (videoPlayer != null)
         {
-            stream.Close();
+            videoPlayer.Stop();
         }
 
-        // create HTTP request
-        
-        HttpWebRequest req = (HttpWebRequest)WebRequest.Create(sourceURL);
-        //Optional (if authorization is Digest)
-        req.Credentials = new NetworkCredential("username", "password");
-        // get response
-        WebResponse resp = req.GetResponse();
-        // get response stream
-        stream = resp.GetResponseStream();
-        StartCoroutine(GetFrame());
+        videoPlayer = gameObject.AddComponent<VideoPlayer>();
+        videoPlayer.url = sourceURL;
+        videoPlayer.isLooping = true;
+        videoPlayer.renderMode = VideoRenderMode.MaterialOverride;
+        videoPlayer.targetMaterialRenderer = frame;
+        videoPlayer.targetMaterialProperty = "_MainTex";
+        videoPlayer.Play();
     }
 
-    IEnumerator GetFrame()
-    {
-        Byte[] JpegData = new Byte[640*480];
+    // IEnumerator GetFrame()
+    // {
+    //     Byte[] JpegData = new Byte[640*480];
 
-        while (true)
-        {
-            int bytesToRead = FindLength(stream);
-            if (textMesh == null)
-            {
-                textMesh = GetComponent<TextMeshPro>();
-                textMesh.text = $"stream is read {bytesToRead}";
-            }
-            print(bytesToRead);
-            if (bytesToRead == -1)
-            {
-                print("End of stream");
-                yield break;
-            }
+    //     while (true)
+    //     {
+    //         int bytesToRead = FindLength(stream);
+    //         if (textMesh == null)
+    //         {
+    //             textMesh = GetComponent<TextMeshPro>();
+    //             textMesh.text = $"stream is read {bytesToRead}";
+    //         }
+    //         print(bytesToRead);
+    //         if (bytesToRead == -1)
+    //         {
+    //             print("End of stream");
+    //             yield break;
+    //         }
 
-            int leftToRead = bytesToRead;
+    //         int leftToRead = bytesToRead;
 
-            while (leftToRead > 0)
-            {
-                leftToRead -= stream.Read(JpegData, bytesToRead - leftToRead, leftToRead);
-                yield return null;
-            }
+    //         while (leftToRead > 0)
+    //         {
+    //             leftToRead -= stream.Read(JpegData, bytesToRead - leftToRead, leftToRead);
+    //             yield return null;
+    //         }
 
-            MemoryStream ms = new MemoryStream(JpegData, 0, bytesToRead, false, true);
+    //         MemoryStream ms = new MemoryStream(JpegData, 0, bytesToRead, false, true);
 
-            texture.LoadImage(ms.GetBuffer());
-            frame.material.mainTexture = texture;
-            stream.ReadByte(); // CR after bytes
-            stream.ReadByte(); // LF after bytes
-        }
-    }
+    //         texture.LoadImage(ms.GetBuffer());
+    //         frame.material.mainTexture = texture;
+    //         stream.ReadByte(); // CR after bytes
+    //         stream.ReadByte(); // LF after bytes
+    //     }
+    // }
 
 
-    int FindLength(Stream stream)
-    {
-        int b;
-        string line = "";
-        int result = -1;
-        bool atEOL = false;
+    // int FindLength(Stream stream)
+    // {
+    //     int b;
+    //     string line = "";
+    //     int result = -1;
+    //     bool atEOL = false;
 
-        while ((b = stream.ReadByte()) != -1)
-        {
-            if (b == 10) continue; // ignore LF char
-            if (b == 13)
-            { // CR
-                if (atEOL)
-                {  // two blank lines means end of header
-                    stream.ReadByte(); // eat last LF
-                    return result;
-                }
-                if (line.StartsWith("Content-Length:"))
-                {
-                    result = Convert.ToInt32(line.Substring("Content-Length:".Length).Trim());
-                }
-                else
-                {
-                    line = "";
-                }
-                atEOL = true;
-            }
-            else
-            {
-                atEOL = false;
-                line += (char)b;
-            }
-        }
-        return -1;
-    }
+    //     while ((b = stream.ReadByte()) != -1)
+    //     {
+    //         if (b == 10) continue; // ignore LF char
+    //         if (b == 13)
+    //         { // CR
+    //             if (atEOL)
+    //             {  // two blank lines means end of header
+    //                 stream.ReadByte(); // eat last LF
+    //                 return result;
+    //             }
+    //             if (line.StartsWith("Content-Length:"))
+    //             {
+    //                 result = Convert.ToInt32(line.Substring("Content-Length:".Length).Trim());
+    //             }
+    //             else
+    //             {
+    //                 line = "";
+    //             }
+    //             atEOL = true;
+    //         }
+    //         else
+    //         {
+    //             atEOL = false;
+    //             line += (char)b;
+    //         }
+    //     }
+    //     return -1;
+    // }
 }
